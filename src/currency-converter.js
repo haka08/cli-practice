bebd88e58dd4986d7de0bcb947acb2bd5bb0f079
haka08 <91915361+haka08@users.noreply.github.com>
2021-11-29 23:21:05 +0800

    Step 1: Capturing user input

--- src/currency-converter.js
+++ src/currency-converter.js
@@ -15,7 +15,9 @@
 
 // We will store each piece of information in a dedicated variable for later use.
 
-
+const amount=process.argv[2];
+const initialCurrency=process.argv[3];
+const targetCurrency=process.argv[4];
 
 // --------------------------------------------------
 // Step 2: Validate user input
